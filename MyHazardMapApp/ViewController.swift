902df5c1961a9516806ed8c692690ef84ccc27b8
IKEchannel <ike25uta@gmail.com>
2020-10-31 16:27:01 +0900

    Initial Commit

---
+++ MyHazardMapApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHazardMapApp
+//
+//  Created by IKEchannel on 2020/10/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
