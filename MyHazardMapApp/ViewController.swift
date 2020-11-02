383c796a83dc955ec8658c7e7e21a9ecd19e2440
IKE channel <ike25uta@gmail.com>
2020-11-02 21:48:26 +0900

    初期データ

--- MyHazardMapApp/ViewController.swift
+++ MyHazardMapApp/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import GoogleMaps
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
+    lazy var mapView = GMSMapView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 37.3318, longitude: -122.0312, zoom: 17.0)
+        mapView = GMSMapView.map(withFrame: CGRect(origin: .zero, size: view.bounds.size), camera: camera)
+        mapView.settings.myLocationButton = true
+        mapView.isMyLocationEnabled = true
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        self.view.addSubview(mapView)
+        self.view.bringSubviewToFront(mapView)
+  }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation = locations.last
+        
+        let camera = GMSCameraPosition.camera(withLatitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude, zoom: 17.0)
+        
+        self.mapView.animate(to: camera)
+        
+        locationManager.stopUpdatingLocation()
+        
     }
-
-
+    
 }
 
